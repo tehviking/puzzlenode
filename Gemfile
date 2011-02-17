cd1e1ca83bca8f4fb73276f008124ba69f045d50
tehviking <brandon.hays@gmail.com>
2011-02-17 16:19:32 -0700

    barebones app, no styling

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ gem 'rails', '~> 3.0.3'
 
 gem 'pg'
 gem 'haml'
-
+gem "compass", ">= 0.10.6"
+gem 'rdiscount'
 gem 'omniauth'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @announcements = Announcement.order('created_at DESC').limit(5)
   end
 
 end
